4a68b529ef6a9237e55cbb6092e6fe736930aee3
Melissa Weber Mendonça <melissawm@gmail.com>
2026-01-16 18:59:26 -0300

    Add initial structure and notebooks

--- contents/conf.py
+++ contents/conf.py
@@ -6,20 +6,35 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+
 project = 'napari surveys'
+html_title = 'napari surveys'
 copyright = '2026, napari team'
 author = 'napari team'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '*.ipynb']
-suffixes = ['.rst', '.md']
+extensions = ["myst_nb"]
+exclude_patterns = ['*.ipynb']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'napari_sphinx_theme'
 html_static_path = ['_static']
+# sidebar content
+html_sidebars = {
+    "**": ["navbar-nav"],
+}
+
+html_theme_options = {
+    "navbar_center": [],
+    "navbar_persistent": [],
+    "secondary_sidebar_items": ["page-toc"],
+}
+
+# use an env var to control whether notebooks are executed
+nb_execution_mode = os.environ.get("NB_EXECUTION_MODE", "force")
+nb_output_stderr = "show"
--- contents/index.md
+++ contents/index.md
@@ -4,8 +4,10 @@ This website contains results and analysis from surveys conducted
 within the napari community.
 
 ```{toctree}
-:maxdepth: 2
+:maxdepth: 1
 
 2023 Survey <2023_survey.md>
+2022 Survey <2022_survey.md>
+2021 Survey <2021_survey.md>
+2020 Survey <2020_survey.md>
 ```
-
